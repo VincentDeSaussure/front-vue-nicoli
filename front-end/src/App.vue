d85ab9bfde39b85540f3370485850ab97a1ed75a
Vincent Deleplanque <vdeleplanque@octo.com>
2019-06-08 17:34:36 +0200

    WIP css element

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Nicoli</span>
-        <span class="font-weight-light"> Charlotte</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <router-link tag="li" to="/cv">
-        <v-img src="assets/img/icone_cv.svg" aspect-ratio="1"></v-img>
-      </router-link>
-      <router-link tag="li" to="/work">
-        <v-img></v-img>
-      </router-link>
-      <li>
-        <a :href="URL.espacePotentielDeVie" target="_blank">espace potentiel de vie</a>
-      </li>
-    </v-toolbar>
+    <div>
+      <header>
+        <h1>
+          Charlotte Nicoli
+        </h1>
+        <v-img :src="IMAGE.CN" aspect-ratio="16:9" height="25vh"></v-img>
+        <ul>
+          <li>
+            <v-img :src="IMAGE.CV" aspect-ratio="1" height="200"></v-img>
+          </li>
+          <li>
+            <v-img :src="IMAGE.WORK" aspect-ratio="1" height="200"></v-img>
+          </li>
+          <li>
+            <v-img :src="IMAGE.CONTACT" aspect-ratio="1" height="200"></v-img>
+          </li>
+        </ul>
+      </header>
+    </div>
 
     <v-content>
       <formulaire-du-seum></formulaire-du-seum>
@@ -24,6 +27,7 @@
 </template>
 
 <script>
+import IMAGE from "./models/imageSrc"
 import URL from "./models/urlDeRedirection"
 import FormulaireDuSeum from "./components/FormulaireDuSeum"
 
@@ -35,8 +39,31 @@ export default {
   },
   data () {
     return {
-      URL
+      URL,
+      IMAGE
     }
   }
 }
 </script>
+<style>
+  h1{
+    color: transparent;
+    height: 5px;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    width: 100px;
+    margin: 0 0px;
+    list-style-type: none;
+  }
+  a {
+    color: #42b983;
+  }
+  img{
+    padding: 10px;
+  }
+</style>
---
+++ front-end/src/models/imageSrc.js
@@ -0,0 +1,13 @@
+import imageCV from "../assets/img/icone_cv.svg"
+import imageCN from "../assets/img/icone_nicoli.svg"
+import imageWORK from "../assets/img/icone_work.svg"
+import imageContact from "../assets/img/icone_contact.svg"
+
+const IMAGE = Object.freeze({
+    CV: imageCV,
+    CN: imageCN,
+    WORK: imageWORK,
+    CONTACT: imageContact
+})
+
+export default IMAGE
