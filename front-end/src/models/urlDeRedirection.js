5eb3d35c60fa52678f46cddc4b40d264d26669d6
Vincent Deleplanque <vdeleplanque@octo.com>
2020-02-08 17:42:16 +0100

    ADD: restart project

--- front-end/src/models/urlDeRedirection.js
+++
@@ -1,6 +0,0 @@
-const URL = Object.freeze({
-    espacePotentielDeVie: "http://potential.lifespaces.art/index.php",
-    nouvelleCollection: "http://www.nouvellecollection.paris/"
-})
-
-export default URL
