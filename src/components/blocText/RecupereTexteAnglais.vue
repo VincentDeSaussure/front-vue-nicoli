b83fd34f2c7671ca98d37c3075b97ff34f2500b0
Vincent Deleplanque <vdeleplanque@octo.com>
2021-05-18 14:34:05 +0200

    indent

--- src/components/blocText/RecupereTexteAnglais.vue
+++ src/components/blocText/RecupereTexteAnglais.vue
@@ -20,32 +20,32 @@ export default {
   components: {BlocTexte},
   props: ["uid"],
   data:() => {
-        return {
-            year: null,
-            dimension: null,
-            description: null,
-            materials: null,
-            subtitle: '',
-            subtitleHyperlink: '',
-            title: null
-        }
-    },
-    methods: {
-        async getContent() {
-            const { data } = await this.$prismic.client.getByUID("bloc-texte-anglais", this.uid)
-            this.year = assureLAttribut(data.year);
-            this.dimension = assureLAttribut(data.dimension);
-            this.description = assureLAttribut(data.description);
-            this.materials = assureLAttribut(data.materials);
-            this.title = assureLAttribut(data.title).toUpperCase();
-            this.subtitle = assureLAttribut(data.subtitle)
-            this.subtitleHyperlink = assureLAttribut(data["subtitle-hyperlink"]);
-        }
-    },
-    watch: {
-        uid() {
-            this.getContent()
-        }
+    return {
+        year: null,
+        dimension: null,
+        description: null,
+        materials: null,
+        subtitle: '',
+        subtitleHyperlink: '',
+        title: null
     }
+  },
+  methods: {
+      async getContent() {
+          const { data } = await this.$prismic.client.getByUID("bloc-texte-anglais", this.uid)
+          this.year = assureLAttribut(data.year);
+          this.dimension = assureLAttribut(data.dimension);
+          this.description = assureLAttribut(data.description);
+          this.materials = assureLAttribut(data.materials);
+          this.title = assureLAttribut(data.title).toUpperCase();
+          this.subtitle = assureLAttribut(data.subtitle)
+          this.subtitleHyperlink = assureLAttribut(data["subtitle-hyperlink"]);
+      }
+  },
+  watch: {
+      uid() {
+          this.getContent()
+      }
+  }
 }
 </script>
